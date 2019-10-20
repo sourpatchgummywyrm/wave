0f7bc408e1a55b1401e676f8168144414de1e720
stephenjuan <stephenjuan11@gmail.com>
2019-10-20 09:27:55 -0400

    fsdf

--- lib/main/profile.dart
+++ lib/main/profile.dart
@@ -27,7 +27,17 @@ class ProfileState extends State<Profile> {
     Size size = MediaQuery.of(context).size;
     return new Scaffold(
       backgroundColor: paleRedColor,
-      body: new Column(
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: new Image.asset(
+              'assets/Girl.png',
+              width: size.width,
+              height: size.height,
+              fit: BoxFit.fill,
+            ),
+          ),
+          new Column(
         children: <Widget>[
           new Padding(
             padding: EdgeInsets.only(top: 30.0),
@@ -96,6 +106,76 @@ class ProfileState extends State<Profile> {
               ))
         ],
       ),
+        ]
+      )/*new Column(
+        children: <Widget>[
+          new Padding(
+            padding: EdgeInsets.only(top: 30.0),
+          ),
+          new Center(
+            child: new CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 50.0,
+            ),
+          ),
+          new Container(
+            padding: EdgeInsets.only(top: 20.0),
+            child: new Text(
+              'Welcome, User',
+              style: loginScreen2,
+            ),
+          ),
+          new Container(
+              padding: EdgeInsets.only(
+                  left: 20.0, right: 20.0, bottom: 20.0, top: 20.0),
+              child: new Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: new ExpansionTile(
+                    title: new Text(
+                      "Attending",
+                      style: new TextStyle(
+                        color: Colors.green,
+                        fontFamily: 'Varela Round',
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    children: <Widget>[
+                      new Text("Event: $eventName , Location: $eventLocation"),
+                      new Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                      )
+                    ],
+                  ))),
+          new SizedBox(
+              width: 300.0,
+              height: 42.5,
+              child: new RaisedButton(
+                elevation: 10.0,
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                color: Colors.white,
+                child: new Row(
+                  children: <Widget>[
+                    new Align(
+                      alignment: Alignment.center,
+                      child: new Icon(
+                        Icons.settings,
+                        color: paleRedColor,
+                      ),
+                    ),
+                    new Align(
+                        alignment: Alignment.center,
+                        child: new Text(
+                          "Settings",
+                          style: loginScreen,
+                        ))
+                  ],
+                ),
+              ))
+        ],
+      ),*/
     );
   }
 }
